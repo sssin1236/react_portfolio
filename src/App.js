cfd04ecfa7d78acf18de1bef245ce18eb0e4ebbd
sssin1236 <suj0358@gmail.com>
2022-02-22 01:39:20 +0900

    gnb 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './css/style.css';
+import Main from './components/Main';
+import About from './components/About';
+import Gallery from './components/Gallery';
+import Youtube from './components/Youtube';
+import Board from './components/Board';
 
 function App() {
   return (
-    
+    <div className="App">
+      <header>
+        <h1>J-STUDIO</h1>
+
+        <ul id="gnb">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </header>
+
+    </div>
   );
 }
 
---
+++ src/components/Youtube.js
@@ -0,0 +1,7 @@
+function Youtube(){
+    return(
+        <div>Youtube</div>
+    )
+}
+
+export default Youtube;
